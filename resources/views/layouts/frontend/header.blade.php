851d24a34d96cdb5080df85f81ba323346b241bc
Nymul Islam Moon <nymulislamlee@gmail.com>
2024-08-01 15:13:12 +0600

    logo header added

--- resources/views/layouts/frontend/header.blade.php
+++ resources/views/layouts/frontend/header.blade.php
@@ -3,6 +3,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Notification</title>
+    <link rel="icon" href="{{ asset('frontend/img/header.jpg') }}" type="image/icon type">
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('frontend/css/style.css') }}"/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
